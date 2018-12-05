e79056c7ad651375826ab646ca8f3976755f4d31
Anderson Carlos Woss <anderson@woss.eng.br>
2018-12-05 12:19:05 -0200

    [Add] Implementada a classe Woss\\Http\\Pipeline

---
+++ src/Pipeline.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Este arquivo percente à biblioteca Woss\\Http.
+ *
+ * @author Anderson Carlos Woss <anderson@woss.eng.br>
+ * @license https://github.com/acwoss/woss-http/blob/master/LICENSE MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Woss\Http;
+
+use InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Traversable;
+
+class Pipeline implements RequestHandlerInterface
+{
+    /**
+     * @var array|Traversable
+     */
+    private $handlers;
+
+    /**
+     * Cria uma nova pipeline para lidar com uma requisição HTTP.
+     *
+     * @param $handlers array|Traversable Conjunto de manipuladores HTTP
+     * @throws InvalidArgumentException Quando o conjunto de entrada não é iterável ou está vazio
+     */
+    public function __construct($handlers)
+    {
+        if (!is_iterable($handlers) || count($handlers) === 0) {
+            throw new InvalidArgumentException('$handlers precisa ser um iterável não vazio');
+        }
+
+        $this->handlers = $handlers;
+    }
+
+    /**
+     * Lida com uma requisição HTTP e produz uma resposta.
+     *
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     * @throws InvalidArgumentException Quando o manipulador é de um tipo inesperado
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $handler = current($this->handlers);
+
+        if (!(
+            $handler instanceof RequestHandlerInterface ||
+            $handler instanceof MiddlewareInterface ||
+            is_callable($handler)
+        )) {
+            throw new InvalidArgumentException(
+                '$handler precisa ser RequestHandlerInterface, MiddlewareInterface ou um objeto chamável'
+            );
+        }
+
+        next($this->handlers);
+
+        if ($handler instanceof RequestHandlerInterface) {
+            return $handler->handle($request);
+        }
+
+        if ($handler instanceof MiddlewareInterface) {
+            return $handler->process($request, $this);
+        }
+
+        return call_user_func($handler, $request, $this);
+    }
+}
