8442c50bd31681ab5b23ed517bfeed8c4fb34d1b
Anderson Carlos Woss <anderson@woss.eng.br>
2018-12-05 23:32:08 -0200

    [Add] Implementada a classe Woss\\Http\\Client\\ClientException

---
+++ src/Client/Client.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Este arquivo percente à biblioteca Woss\\Http.
+ *
+ * @author Anderson Carlos Woss <anderson@woss.eng.br>
+ * @license https://github.com/acwoss/woss-http/blob/master/LICENSE MIT License
+ */
+declare(strict_types=1);
+
+namespace Woss\Http\Client;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class Client implements ClientInterface
+{
+    /**
+     * Sends a PSR-7 request and returns a PSR-7 response.
+     *
+     * @param RequestInterface $request
+     *
+     * @return ResponseInterface
+     *
+     * @throws \Psr\Http\Client\ClientExceptionInterface If an error happens while processing the request.
+     */
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        // TODO: Implement sendRequest() method.
+    }
+}
