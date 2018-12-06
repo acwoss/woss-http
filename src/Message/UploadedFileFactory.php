1f59588f438ce265c6bab3f9354669eeb7b6a0bb
Anderson Carlos Woss <anderson@woss.eng.br>
2018-12-05 23:31:43 -0200

    [Add] Implementada a classe Woss\\Http\\Message\\UriFactory

---
+++ src/Message/UploadedFileFactory.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Este arquivo percente à biblioteca Woss\\Http.
+ *
+ * @author Anderson Carlos Woss <anderson@woss.eng.br>
+ * @license https://github.com/acwoss/woss-http/blob/master/LICENSE MIT License
+ */
+declare(strict_types=1);
+
+namespace Woss\Http\Message;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UploadedFileInterface;
+
+class UploadedFileFactory implements UploadedFileFactoryInterface
+{
+    /**
+     * Create a new uploaded file.
+     *
+     * If a size is not provided it will be determined by checking the size of
+     * the file.
+     *
+     * @see http://php.net/manual/features.file-upload.post-method.php
+     * @see http://php.net/manual/features.file-upload.errors.php
+     *
+     * @param StreamInterface $stream Underlying stream representing the
+     *     uploaded file content.
+     * @param int $size in bytes
+     * @param int $error PHP file upload error
+     * @param string $clientFilename Filename as provided by the client, if any.
+     * @param string $clientMediaType Media type as provided by the client, if any.
+     *
+     * @return UploadedFileInterface
+     *
+     * @throws \InvalidArgumentException If the file resource is not readable.
+     */
+    public function createUploadedFile(
+        StreamInterface $stream,
+        int $size = null,
+        int $error = \UPLOAD_ERR_OK,
+        string $clientFilename = null,
+        string $clientMediaType = null
+    ): UploadedFileInterface
+    {
+        // TODO: Implement createUploadedFile() method.
+    }
+}
---
+++ src/Message/UriFactory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Este arquivo percente à biblioteca Woss\\Http.
+ *
+ * @author Anderson Carlos Woss <anderson@woss.eng.br>
+ * @license https://github.com/acwoss/woss-http/blob/master/LICENSE MIT License
+ */
+declare(strict_types=1);
+
+namespace Woss\Http\Message;
+
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+class UriFactory implements UriFactoryInterface
+{
+    /**
+     * Create a new URI.
+     *
+     * @param string $uri
+     *
+     * @return UriInterface
+     *
+     * @throws \InvalidArgumentException If the given URI cannot be parsed.
+     */
+    public function createUri(string $uri = ''): UriInterface
+    {
+        // TODO: Implement createUri() method.
+    }
+}
