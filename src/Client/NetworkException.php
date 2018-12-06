64269351940485ba8ee1987f8628a8605a44bef4
Anderson Carlos Woss <anderson@woss.eng.br>
2018-12-05 23:32:29 -0200

    [Add] Implementada a classe Woss\\Http\\Client\\RequestException

---
+++ src/Client/NetworkException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Este arquivo percente à biblioteca Woss\\Http.
+ *
+ * @author Anderson Carlos Woss <anderson@woss.eng.br>
+ * @license https://github.com/acwoss/woss-http/blob/master/LICENSE MIT License
+ */
+declare(strict_types=1);
+
+namespace Woss\Http\Client;
+
+use Exception;
+use Psr\Http\Client\NetworkExceptionInterface;
+use Psr\Http\Message\RequestInterface;
+
+class NetworkException extends Exception implements NetworkExceptionInterface
+{
+    /**
+     * Returns the request.
+     *
+     * The request object MAY be a different object from the one passed to ClientInterface::sendRequest()
+     *
+     * @return RequestInterface
+     */
+    public function getRequest(): RequestInterface
+    {
+        // TODO: Implement getRequest() method.
+    }
+}
