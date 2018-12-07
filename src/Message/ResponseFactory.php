be1f82daeb0b1d1b76f30eb5f2a9aca7c05212da
Anderson Carlos Woss <anderson@woss.eng.br>
2018-12-07 16:41:49 -0200

    [Mod] Modificada a classe Woss\\Http\\Message\\ResponseFactory conforme padrões

--- src/Message/ResponseFactory.php
+++ src/Message/ResponseFactory.php
@@ -15,17 +15,10 @@ use Psr\Http\Message\ResponseInterface;
 class ResponseFactory implements ResponseFactoryInterface
 {
     /**
-     * Create a new response.
-     *
-     * @param int $code HTTP status code; defaults to 200
-     * @param string $reasonPhrase Reason phrase to associate with status code
-     *     in generated response; if none is provided implementations MAY use
-     *     the defaults as suggested in the HTTP specification.
-     *
-     * @return ResponseInterface
+     * {@inheritDoc}
      */
     public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
     {
-        // TODO: Implement createResponse() method.
+        return (new Response())->withStatus($code, $reasonPhrase);
     }
 }
