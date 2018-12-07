d9e249a673c2822de55dd4887197905be8b8796d
Anderson Carlos Woss <anderson@woss.eng.br>
2018-12-07 16:41:16 -0200

    [Mod] Implementada a classe Woss\\Http\\Message\\RequestFactory

--- src/Message/RequestFactory.php
+++ src/Message/RequestFactory.php
@@ -11,22 +11,14 @@ namespace Woss\Http\Message;
 
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\UriInterface;
 
 class RequestFactory implements RequestFactoryInterface
 {
     /**
-     * Create a new request.
-     *
-     * @param string $method The HTTP method associated with the request.
-     * @param UriInterface|string $uri The URI associated with the request. If
-     *     the value is a string, the factory MUST create a UriInterface
-     *     instance based on it.
-     *
-     * @return RequestInterface
+     * {@inheritDoc}
      */
     public function createRequest(string $method, $uri): RequestInterface
     {
-        // TODO: Implement createRequest() method.
+        return new Request($uri, $method);
     }
 }
